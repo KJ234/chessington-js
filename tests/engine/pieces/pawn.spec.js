4850344407e40f06ab87fcdcbb986c25535ed343
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-10-06 12:39:53 +0100

    Added a test for pawns moving one square

--- tests/engine/pieces/pawn.spec.js
+++ tests/engine/pieces/pawn.spec.js
@@ -1,4 +1,8 @@
+import 'chai/register-should';
 import Pawn from '../../../src/engine/pieces/pawn';
+import Board from '../../../src/engine/board';
+import Player from '../../../src/engine/player';
+import Square from '../../../src/engine/square';
 
 
 describe('Pawn', () => {
