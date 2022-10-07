a609d37d66720dd064438462f117968668c64833
KJ234 <komaljaved109@gmail.com>
2022-10-07 14:33:16 +0100

    changes

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -1,4 +1,8 @@
 import Piece from './piece';
+import player from '../player';
+import Square from '../square';
+
+// pawn is a child of piece
 
 export default class Pawn extends Piece {
     constructor(player) {
@@ -6,6 +10,25 @@ export default class Pawn extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let location = board.findPiece(this)
+        let moves = []
+        if (this.player === player.WHITE) {
+            if (location.row < 1) {
+                moves.push(Square.at(location.row + 1, location.col))
+            } else {
+
+            } moves.push(Square.at(location.row + 1, location.col))
+        }
+
+    if (this.player === player.BLACK) {
+        if(location.row >= 6) {
+            moves.push(Square.at(location.row - 1, location.col))
+            
+        } else {
+            moves.push(Square.at(location.row - 1, location.col));
+        }
     }
+    return moves;
+    
+}
 }
